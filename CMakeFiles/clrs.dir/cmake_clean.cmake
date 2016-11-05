3a1e0e17427bb3520ff26aefc5ca5a396405b482
Okeowo <okeowoaderemi@gmail.com>
2016-11-05 19:59:10 +0100

    Installed CMake to build project for practice

---
+++ CMakeFiles/clrs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clrs.dir/Insertion_sort.cpp.o"
+  "clrs.pdb"
+  "clrs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clrs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
