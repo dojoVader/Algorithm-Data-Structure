0be4bba7077b79199ce6fedea0b1ca69eb8662e8
Okeowo <okeowoaderemi@gmail.com>
2017-12-03 17:33:53 +0100

    Added Array Rotation using the Modulus Operator

---
+++ Array/ArrayRotation.java
@@ -0,0 +1,35 @@
+class ArrayRotation{
+
+	static int[] rotateArray(int[] arrayList, int rotation){
+	 int[] tempArray=new int[arrayList.length]; //Initiate the array by number of rotation
+	 //We need to break the 
+	 for(int count=0; count < tempArray.length; count++){
+	 	//Using the Modulus Operator let's create the Array
+	 	int tempValue = arrayList[count];
+	 	//This gets Index of the Array 
+	 	int newValue = (((tempValue % arrayList.length) - 1) - (rotation % arrayList.length));
+	 	newValue = ((arrayList.length + (newValue)) % arrayList.length);
+	 	tempArray[newValue]=tempValue;
+
+	 }
+	 return tempArray;
+	}
+
+    public static void main(String args[]){
+        /* Driver program to check above functions */
+
+	   // Let us search 3 in below array
+	   int arr[] = {1,2,3,4,5,6, 7};
+	   int rotation_index=8;  
+	   int newArray[] = rotateArray(arr,rotation_index);
+
+	   //Print the values
+
+	   for(int count=0; count < newArray.length; count++){
+	   	System.out.println(newArray[count]+"\t");
+	   }
+	   
+	  
+   }
+    
+}
